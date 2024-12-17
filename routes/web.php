1c100c0311141147e82e2dc4374b1c591a65113a
jordimaano <jordidimaano@gmail.com>
2024-12-17 23:34:49 +0800

    Removed commented codes

--- routes/web.php
+++ routes/web.php
@@ -30,29 +30,19 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 //needs to be logged in
 Route::middleware(['auth'])->group(function () {
     //boarding house
-    // Route::get('/boarding-houses', [BoardingHouseController::class, 'index'])->name('boarding-houses.index'); // List all
     Route::get('/boarding-houses/create', [BoardingHouseController::class, 'create'])->name('boarding-houses.create'); // Show form to create
     Route::post('/boarding-houses', [BoardingHouseController::class, 'store'])->name('boarding-houses.store'); // Save new record
     Route::get('/boarding-houses/{boardingHouse}', [BoardingHouseController::class, 'show'])->name('boarding-houses.show'); // Show details
-    // Route::get('/boarding-houses/{id}/edit', [BoardingHouseController::class, 'edit'])->name('boarding-houses.edit'); // Show form to edit
-    // Route::put('/boarding-houses/{id}', [BoardingHouseController::class, 'update'])->name('boarding-houses.update'); // Update record
-    // Route::delete('/boarding-houses/{id}', [BoardingHouseController::class, 'destroy'])->name('boarding-houses.destroy'); // Delete record
 
     //rooms
     Route::get('/rooms', [RoomController::class, 'index'])->name('rooms.index'); // List all
     Route::get('/boarding-houses/{boardingHouse}/rooms/create', [RoomController::class, 'create'])->name('rooms.create'); // Show form to create
     Route::post('/rooms/store', [RoomController::class, 'store'])->name('rooms.store'); // Save new record
     Route::get('/rooms/{room}', [RoomController::class, 'show'])->name('rooms.show'); // Show details
-    // Route::get('/rooms/{room}/edit', [RoomController::class, 'edit'])->name('rooms.edit'); // Show form to edit
-    // Route::put('/rooms/{room}', [RoomController::class, 'update'])->name('rooms.update'); // Update record
-    // Route::delete('/rooms/{room}', [RoomController::class, 'destroy'])->name('rooms.destroy'); // Delete record
 
     //boarders
     Route::get('/boarders', [BoarderController::class, 'index'])->name('boarders.index'); // List all
     Route::get('/boarding-houses/{boardingHouse}/boarders/create', [BoarderController::class, 'create'])->name('boarders.create'); // Show form to create
     Route::post('/boarders/store', [BoarderController::class, 'store'])->name('boarders.store'); // Save new record
     Route::get('/boarders', [BoarderController::class, 'show'])->name('boarders.show'); // Show details
-    // Route::get('/boarders/{room}/edit', [BoarderController::class, 'edit'])->name('boarders.edit'); // Show form to edit
-    // Route::put('/boarders/{room}', [BoarderController::class, 'update'])->name('boarders.update'); // Update record
-    // Route::delete('/boarders/{room}', [BoarderController::class, 'destroy'])->name('boarders.destroy'); // Delete record
 });
